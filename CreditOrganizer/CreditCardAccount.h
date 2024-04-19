2f5976bc16f2283d4b9c0f3f507e3ec44fabd53a
BraggerSchos <147817212+BraggerSchos@users.noreply.github.com>
2024-04-19 15:45:58 +0300

    Add files via upload

---
+++ CreditOrganizer/CreditCardAccount.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "pch.h"
+#include "LoyaltyScheme.h"
+
+ref class CreditCardAccount
+{
+public:
+	CreditCardAccount(long number, double limit);
+	void SetCreditLimit(double amount);
+	bool MakePurchase(double amount);
+	void MakeRepayment(double amount);
+	void PrintStatement();
+	long GetAccountNumber();
+	static int GetNumberOfAccounts();
+	static CreditCardAccount();
+	literal System::String^ name = "Super Platinum Card";
+	void RedeemLoyaltyPoints();
+
+private:
+	initonly long accountNumber;;
+	double currentBalance;
+	double creditLimit;
+	static int numberOfAccounts = 0;
+	static double interestRate;
+	LoyaltyScheme^ scheme;
+	
+};
